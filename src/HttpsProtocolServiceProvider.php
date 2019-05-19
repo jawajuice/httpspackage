b819539cacc2eaa1bc4991d4ea1a844ee5e7ea5d
Matic <matic@wirelab.nl>
2019-05-19 09:49:32 +0200

    composer file correction

--- src/HttpsProtocolServiceProvider.php
+++ src/HttpsProtocolServiceProvider.php
@@ -5,12 +5,12 @@ namespace Wirelab\HttpsProtocol;
     class HttpsProtocolServiceProvider extends ServiceProvider {
         public function boot()
         {
+            //register middleware in kernel
             $router = $this->app['router'];
             $router->pushMiddlewareToGroup('web', HttpsProtocol::class);
         }
         public function register()
         {
-            //$router = $this->app['router'];
         }
     }
     ?>
